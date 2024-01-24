546206ce64a2091c17422ae4f8556f9ac4481d3e
Dmeng <meng35922123@gmail.com>
2024-01-24 15:25:13 +0800

    zoom

--- Sources/LazyPager/ViewDataProvider.swift
+++ Sources/LazyPager/ViewDataProvider.swift
@@ -63,7 +63,7 @@ public class ViewDataProvider<Content: View, DataCollecton: RandomAccessCollecti
         guard let dta = data[safe: zoomableView.index] else { return }
         zoomableView.hostingController.rootView = viewLoader(dta)
         zoomableView.config = config
-//        zoomableView.zoom(at: zoomableView.center, scale: config.minZoom,animated: false)
+        zoomableView.zoomMinScale(at: zoomableView.center, scale: config.minZoom,animated: true)
     }
     
     // MARK: UIViewController
